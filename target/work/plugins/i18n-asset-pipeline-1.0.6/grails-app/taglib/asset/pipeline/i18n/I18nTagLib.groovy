910fcdd5d8d48f2f8e3629407c02d6b084d6a16f
Aarif <mohd.aarif@tothenew.com>
2016-04-26 15:54:38 +0530

    internationalize js messages

---
+++ target/work/plugins/i18n-asset-pipeline-1.0.6/grails-app/taglib/asset/pipeline/i18n/I18nTagLib.groovy
@@ -0,0 +1,122 @@
+/*
+ * I18nTagLib.groovy
+ *
+ * Copyright (c) 2014-2015, Daniel Ellermann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package asset.pipeline.i18n
+
+import asset.pipeline.AssetFile
+import asset.pipeline.AssetHelper
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.springframework.core.io.Resource
+
+
+/**
+ * Class {@code I18nTagLib} contains tags that help loading client-side i18n
+ * files.
+ *
+ * @author  Daniel Ellermann
+ * @version 1.0
+ */
+class I18nTagLib {
+
+    //-- Class variables ------------------------
+
+    static namespace = "asset"
+
+
+    //-- Instance variables ---------------------
+
+    GrailsApplication grailsApplication
+
+
+    //-- Public methods -------------------------
+
+    /**
+     * Includes a JavaScript asset that provides client-side i18n for the given
+     * locale.
+     *
+     * @attr locale the given locale
+     * @attr [name] the name of the i18n file without extension; defaults to "messages"
+     */
+    def i18n = { attrs ->
+        def l = attrs.remove('locale') ?: ''
+        String locale = ''
+        if (l instanceof Locale) {
+            locale = l.toString()
+        } else if (l instanceof CharSequence) {
+            locale = l
+        } else {
+            if (log.warnEnabled) {
+                log.warn "Unknown type ${l.class.name} for attribute 'locale'; use default locale."
+            }
+        }
+        locale = locale.replace('-', '_')
+        if (log.debugEnabled) {
+            log.debug "Retrieving i18n messages for locale ${locale}…"
+        }
+
+        String name = attrs.remove('name') ?: 'messages'
+        String [] parts = locale.split('_')
+
+        String src = null
+        for (int i = parts.length - 1; i >= 0 && !src; --i) {
+            StringBuilder buf = new StringBuilder(name)
+            for (int j = 0; j <= i; j++) {
+                buf << '_' << parts[j]
+            }
+            String s = buf.toString()
+            if (log.debugEnabled) {
+                log.debug "Trying to find asset ${s}…"
+            }
+
+            /*
+             * XXX This is a somewhat dirty hack.  When running in WAR file a
+             * filter (asset.pipeline.AssetPipelineFilter) looks for a resource
+             * in folder "assets".  So we try this first, and, if not found, we
+             * look in "grails-app/assets" via fileForUri().
+             */
+            Resource res =
+                grailsApplication.mainContext.getResource("assets/${s}.js")
+            if (res.exists()) {
+                src = s
+                break
+            } else {
+                res = grailsApplication.mainContext.getResource(
+                    "assets/${s}.unminified.js"
+                )
+                if (res.exists()) {
+                    src = s
+                    break
+                } else {
+                    AssetFile f =
+                        AssetHelper.fileForUri(s, 'application/javascript')
+                    if (f != null) {
+                        src = s
+                        break
+                    }
+                }
+            }
+        }
+        if (log.debugEnabled) {
+            log.debug "Found asset ${src ?: name}"
+        }
+
+        out << asset.javascript(src: src ?: name)
+    }
+}
+
---
+++ target/work/plugins/i18n-asset-pipeline-1.0.6/src/groovy/asset/pipeline/i18n/I18nPreprocessor.groovy
@@ -0,0 +1,143 @@
+/*
+ * I18nPreprocessor.groovy
+ *
+ * Copyright (c) 2014-2015, Daniel Ellermann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package asset.pipeline.i18n
+
+import asset.pipeline.AssetFile
+import asset.pipeline.AssetHelper
+import groovy.transform.TypeChecked
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+
+/**
+ * The class {@code I18nPreprocessor} represents a pre-processor for i18n files
+ * which are used in the asset pipeline.
+ *
+ * @author  Daniel Ellermann
+ * @author  David Estes
+ * @version 1.0
+ */
+@TypeChecked
+class I18nPreprocessor {
+
+    //-- Constants ------------------------------
+
+    protected static final Pattern REGEX_IGNORE = ~/^\s*(?:#.*)?$/
+    protected static final Pattern REGEX_IMPORT = ~/^\s*@import\s+(.+)$/
+
+
+    //-- Constructors ---------------------------
+
+    protected I18nPreprocessor() {}
+
+
+    //-- Public methods -------------------------
+
+    /**
+     * Gets the one and only factory instance.
+     *
+     * @return  the singleton instance of this factory
+     */
+    static I18nPreprocessor getInstance() {
+        InstanceHolder.INSTANCE
+    }
+
+    /**
+     * Pre-processes the given i18n file by removing empty lines and comment
+     * lines and resolving all imports.
+     *
+     * @param file  the given i18n file
+     * @param input the content of the i18n file
+     * @return      the pre-processed content
+     */
+    String preprocess(AssetFile file, String input = file.inputStream.text) {
+        Set<AssetFile> fileHistory = new HashSet<>()
+        fileHistory << file
+        preprocess input, fileHistory
+    }
+
+
+    //-- Non-public methods ---------------------
+
+    /**
+     * Pre-processes an i18n file by removing empty lines and comment lines and
+     * resolving all imports.
+     *
+     * @param input         the content of the i18n file
+     * @param fileHistory   the history of all import files that have been
+     *                      processed already; this is needed to handle
+     *                      circular dependencies
+     * @return              the pre-processed content
+     */
+    protected String preprocess(String input, Set<AssetFile> fileHistory) {
+        StringBuffer buf = new StringBuffer(input.length())
+        input.eachLine { String line ->
+            line = line.trim()
+            if (line ==~ REGEX_IGNORE) return
+
+            Matcher m = line =~ REGEX_IMPORT
+            if (m) {
+                line = resolveImport(m.group(1).trim(), fileHistory)
+                if (!line) return
+                line = line.trim()
+            }
+            buf << line << '\n'
+        }
+
+        buf.toString()
+    }
+
+    /**
+     * Loads the import file with the file name and processes its content.
+     *
+     * @param fileName      the name of the import file
+     * @param fileHistory   the history of all import files that have been
+     *                      processed already; this is needed to handle
+     *                      circular dependencies
+     * @return              the pre-processed content of the import file
+     */
+    protected String resolveImport(String fileName,
+                                   Set<AssetFile> fileHistory)
+    {
+        if (!fileName.endsWith('.i18n')) {
+            fileName += '.i18n'
+        }
+
+        AssetFile importFile =
+            (AssetFile) AssetHelper.fileForFullName(fileName)
+        if (importFile == null || importFile in fileHistory) {
+            return ''
+        }
+
+        fileHistory << importFile
+        preprocess importFile.inputStream.text, fileHistory
+    }
+
+
+    //-- Inner classes --------------------------
+
+    private static class InstanceHolder {
+
+        //-- Constants --------------------------
+
+        public static final I18nPreprocessor INSTANCE =
+            new I18nPreprocessor()
+    }
+}
