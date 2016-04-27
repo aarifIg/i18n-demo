40f8f84965a2e2ed302e0e0e8af6aa59b7000d1a
Aarif <mohd.aarif@tothenew.com>
2016-04-27 12:03:08 +0530

    adding default locale and git ignore

--- grails-app/controllers/i18n/demo/UserController.groovy
+++ grails-app/controllers/i18n/demo/UserController.groovy
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.support.RequestContextUtils
 class UserController {
 
     def messageSource
+    def index(){
+
+    }
 
     def login() {
         Locale locale=RequestContextUtils.getLocale(request)
-        String msg=messageSource.getMessage("user.login.success.message", [] as Object[], locale)
+        String msg=messageSource.getMessage("user.login.success.message", null, locale)
         render msg
     }
 }
